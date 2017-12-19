c8486d4646462c2f16e08a9314e85e99adb81f9a
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-19 15:51:53 -0500

    Guess-list state complete.

--- src/components/guess-list.js
+++ src/components/guess-list.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 import './guess-list.css';
 
-export default function GuessList(props) {
-  const guesses = props.guesses.map((guess, index) => (
-    <li key={index}>
-      {guess}
-    </li>
-  ));
+export class GuessList extends React.Component {
 
-  return (
-    <ul id="guessList" className="guessBox clearfix">
-      {guesses}
-    </ul>
-  );
+  render(){
+    const guesses = this.props.guesses.map((guess, index) => (
+      <li key={index}>
+        {guess}
+      </li>
+    ));
+    return (
+      <ul id="guessList" className="guessBox clearfix">
+        {guesses}
+      </ul>
+    );
+  }
 }
+
+const mapStateToProps = (state, props) =>{
+  return{
+    guesses: state.guesses
+  }
+};
+
+export default connect(mapStateToProps)(GuessList);
